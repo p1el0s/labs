6716353ede3128ea85b5871dd3b87b3caeb1096f
p1el0s <92648324+p1el0s@users.noreply.github.com>
2022-04-03 22:33:32 +0300

    Add files via upload

---
+++ Java/FullMatrix/MirrorMatrix.java
@@ -0,0 +1,15 @@
+package third.matrix;
+public class MirrorMatrix extends Matrix {
+	public MirrorMatrix(int n, int c) {
+		super(n, 1);
+		column = c;
+	}
+
+	public void setElement(int i, int c, int b) {
+		arr[i][0] = b;
+	}
+
+	public int getElement(int b, int c) {
+		return arr[b][0];
+	}
+}
---
+++ Java/FullMatrix/SquareMatrix.java
@@ -0,0 +1,41 @@
+package third.matrix;
+import third.exception.*;
+public class SquareMatrix extends Matrix{
+	private int size;
+
+	public SquareMatrix(int size) {
+		super(size,size);
+		this.size = size;
+		for(int i = 0; i < size; i++)
+			for(int z = 0; z < size; z++)
+				if(i == z) this.setElement(i,i,1);
+	}
+
+	final public SquareMatrix sum(SquareMatrix a) throws MyException{
+		if(this.size != a.size) {
+			throw new MyException("Matrix one not similar two.");
+		}
+		SquareMatrix temp = new SquareMatrix(a.size);
+		temp.clearMatrix();
+		for(int i = 0; i < size; i++) {
+			for(int z = 0; z < size; z++) {
+				int val = temp.getElement(i, z);
+				val += this.getElement(i, z) + a.getElement(i, z);
+				temp.setElement(i, z, val);
+			}
+		}
+		return temp;
+	}
+
+	final public void clearMatrix() {
+		for(int i = 0; i < size; i++)
+			for(int b = 0; b < size; b++)
+				this.setElement(i,b,0);
+	}
+}
+
+/*
+
+Mirror. 1 СТОЛБЕЦ ХРАНИТЬ ВЫВОДИТЬ ВСЕ ОСТАЛЬНЫЕ.
+
+*/
